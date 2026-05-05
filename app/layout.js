a99e7f910407a1b84e15c7f64326464a431ac04d
Yotrages <qayyumoladimeji@gmail.com>
2026-05-05 22:44:22 +0100

    fixes

--- app/layout.js
+++ app/layout.js
@@ -16,20 +16,30 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Replace YOUR_PIXEL_ID with your real Meta Pixel ID */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){
-              n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;
-              s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
-              (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init','YOUR_PIXEL_ID');fbq('track','PageView');
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '967389422604324');
+              fbq('track', 'PageView');
             `,
           }}
         />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=967389422604324&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
